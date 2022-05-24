71a70a6cae3198ecfae232bf3336d8ce30cf800a
unknown <technicalsalma@gmail.com>
2022-05-24 18:41:06 +0600

    usere email ADD

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ async function run(){
       $set: user,
     };
     const result = await userCollection.updateOne(filter, updateDoc,options);
-    res.send(result)
+    const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '48h'});
+    res.send({result, token})
   }) 
 
   
